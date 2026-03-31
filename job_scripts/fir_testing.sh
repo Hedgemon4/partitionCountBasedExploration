27b017c640d355ecf37dbd8c77cc5371eaa02117
Seth Akins <sethlinc4@gmail.com>
2026-03-31 15:46:33 -0600

    change pip install command

--- job_scripts/fir_testing.sh
+++ job_scripts/fir_testing.sh
@@ -11,6 +11,6 @@ virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 python -m pip install --no-index --upgrade pip
 
-python -m pip install -U --no-index -r requirements.txt
+python -m pip install -U -r requirements.txt
 
 python pqn_with_counts.py
