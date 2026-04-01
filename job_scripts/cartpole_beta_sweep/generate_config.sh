fd49178996c229c89c270492dd856ca16538735c
Seth Akins <sethlinc4@gmail.com>
2026-04-01 10:19:16 -0600

    Fix metrics being saved to same folder

--- job_scripts/cartpole_beta_sweep/generate_config.sh
+++ job_scripts/cartpole_beta_sweep/generate_config.sh
@@ -5,5 +5,5 @@ betas=(0.05 0.01 0.001)
 output_file="configs.txt"
 >"$output_file"
 for beta in "${betas[@]}"; do
-    echo "--beta $beta" >>"$output_file"
+    echo "--beta $beta" --metrics_folder_name cartpole_beta_sweep/beta_$beta >>"$output_file"
 done
