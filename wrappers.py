1c46de1d95f239dbf43e82cc1c5f00035558cc07
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2026-04-22 14:30:30 -0600

    feat: use navix gymnax wrapper

--- wrappers.py
+++ wrappers.py
@@ -203,3 +203,30 @@ class NavixGymnaxWrapper:
         reward = new_timestep.reward
         done = new_timestep.is_done()
         return obs, new_timestep, reward, done, {}
+    
+
+
+class NavixGymnaxWrapper:
+    def __init__(self, navix_env):
+        self._env = navix_env
+
+    def reset(self, key, params=None):
+        timestep = self._env.reset(key)
+        return timestep.observation, timestep
+
+    def step(self, key, state, action, params=None):
+        timestep = self._env.step(state, action)
+        return timestep.observation, timestep, timestep.reward, timestep.is_done(), {}
+
+    def observation_space(self, params):
+        return spaces.Box(
+            low=self._env.observation_space.minimum,
+            high=self._env.observation_space.maximum,
+            shape=(np.prod(self._env.observation_space.shape),),
+            dtype=self._env.observation_space.dtype,
+        )
+
+    def action_space(self, params):
+        return spaces.Discrete(
+            num_categories=self._env.action_space.maximum.item() + 1,
+        )
