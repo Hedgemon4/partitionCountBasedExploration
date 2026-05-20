4e6ba7475bcdc5b542a8dda3b08b8202059766d7
Seth Akins <sethlinc4@gmail.com>
2026-05-20 14:01:02 -0600

    modifying requirements

--- job_scripts/atari/submit.sh
+++ job_scripts/atari/submit.sh
@@ -6,13 +6,17 @@
 #SBATCH --gpus=h100_2g.20gb:1
 
 module load python/3.12.4
+module load opencv
+module load cmake
+module load cuda/12.9
 
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 python -m pip install --no-index --upgrade pip
 
-python -m pip install -U -r requirements.txt
+python -m pip install -U -r requirements.txt --no-index -f wheels/
 
-python -m pip install ale/
+python -m pip install ale/ --no-index
+ln -s -f /home/slakins/links/scratch/projects/partitionCountBasedExploration/roms/ $SLURM_TMPDIR/env/lib/python3.12/site-packages/ale_py/roms/
 
 python pqn_atari_with_counts.py
