1aa79fbb55a6d71b69e76e4d500f637c1919c0c1
Seth Akins <sethlinc4@gmail.com>
2026-05-26 16:12:41 -0600

    reduce job time

--- job_scripts/atari/seaquest_baseline/submit.sh
+++ job_scripts/atari/seaquest_baseline/submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=aip-mbowling
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16GB
-#SBATCH --time=08:59:00
+#SBATCH --time=02:59:00
 #SBATCH --gpus=1
 #SBATCH --mail-user=slakins@ualberta.ca
 #SBATCH --mail-type=ALL
