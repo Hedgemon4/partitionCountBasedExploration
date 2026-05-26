446ac5ef4413870cf0f53c30daa7cbb003aa9bb1
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2026-05-25 23:52:37 -0600

    feat: Fix submit file for vulcan

--- job_scripts/craftax_symbolic_sweep/submit.sh
+++ job_scripts/craftax_symbolic_sweep/submit.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH --account=rrg-mbowling-ad
+#SBATCH --account=aip-mbowling
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16000M
+#SBATCH --gpus=1
+#SBATCH --mem=64G
 #SBATCH --time=02:59:00
 
 module load python/3.12.4
 
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-python -m pip install --no-index --upgrade pip
+mkdir $SLURM_TMPDIR/$SLURM_JOB_ID
+cp ~/partitionCountBasedExploration/venv_gpu.tar.xz $SLURM_TMPDIR/$SLURM_JOB_ID
+tar -xf $SLURM_TMPDIR/$SLURM_JOB_ID/venv_gpu.tar.xz -C $SLURM_TMPDIR/$SLURM_JOB_ID
 
-python -m pip install -U -r requirements.txt
+
 
 PARAMS=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" "$CONFIG_PATH")
 
-python pqn_with_counts.py craftax $PARAMS
- 
+$SLURM_TMPDIR/$SLURM_JOB_ID/.venv/bin/python3.12 ~/partitionCountBasedExploration/pqn_with_counts.py craftax $PARAMS
+
+
