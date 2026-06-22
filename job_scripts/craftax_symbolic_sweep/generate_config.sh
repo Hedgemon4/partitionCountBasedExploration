8232551cd55de87e827763b1bee170dadabeadd5
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2026-06-22 10:08:06 -0600

    exp: Write sweep script for craftax

--- job_scripts/craftax_symbolic_sweep/generate_config.sh
+++ job_scripts/craftax_symbolic_sweep/generate_config.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
 # Sweep Options
-betas=(0.0 0.1)
-activations=(fta fta-original)
-max_grad_norm=(10.0 100.0)
-episode_lengths=(200 1000)
+betas=(0.0 0.001 0.005 0.01 0.05 0.1)
 
 output_file="configs.txt"
 
 >"$output_file"
 for beta in "${betas[@]}"; do
-    for activation in "${activations[@]}"; do
-        for max_norm in "${max_grad_norm[@]}"; do
-            for length in "${episode_lengths[@]}"; do
-                echo "--beta $beta --max-grad-norm $max_norm --episode_length $length network.activation2:$activation --output-folder-name mountaincar_baseline/beta_"$beta"__max_grad_norm_"$max_norm"__episode_length_"$length"__"$activation"" >>"$output_file"
-            done
-        done
-    done
+        echo "--beta $beta --output-folder-name craftax_symbolic/beta_"$beta"__fta_first_layer" >>"$output_file"
 done
