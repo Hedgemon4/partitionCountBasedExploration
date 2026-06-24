277f18ea183e557dab31d69794a4f62c8029aaa4
Seth Akins <sethlinc4@gmail.com>
2026-06-24 14:15:20 -0600

    changed name

--- job_scripts/atari/amidar_counts_sweep/generate_config.sh
+++ job_scripts/atari/amidar_counts_sweep/generate_config.sh
@@ -9,6 +9,6 @@ output_file="configs.txt"
 >"$output_file"
 for beta in "${betas[@]}"; do
     for next in "${next_state[@]}"; do
-        echo "--environment amidar --force-xla --beta $beta --network.next_state_coef $next --output-folder-name amidar_sweep/beta_${beta}_next_${next}/seed_SEED" >>"$output_file"
+        echo "--environment amidar --force-xla --beta $beta --network.next_state_coef $next --output-folder-name amidar_counts_sweep/beta_${beta}_next_${next}/seed_SEED" >>"$output_file"
     done
 done
