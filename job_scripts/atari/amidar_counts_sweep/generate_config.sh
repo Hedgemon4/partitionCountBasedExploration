4218ebbe2eafc76dc75ac80a32414083e34f7190
Seth Akins <sethlinc4@gmail.com>
2026-06-24 14:14:40 -0600

    add sweep for amidar

---
+++ job_scripts/atari/amidar_counts_sweep/generate_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Sweep Options
+betas=(0.0 0.1 0.25 0.5 1.0)
+next_state=(0.0 0.25 0.5 1.0)
+
+output_file="configs.txt"
+
+>"$output_file"
+for beta in "${betas[@]}"; do
+    for next in "${next_state[@]}"; do
+        echo "--environment amidar --force-xla --beta $beta --network.next_state_coef $next --output-folder-name amidar_sweep/beta_${beta}_next_${next}/seed_SEED" >>"$output_file"
+    done
+done
