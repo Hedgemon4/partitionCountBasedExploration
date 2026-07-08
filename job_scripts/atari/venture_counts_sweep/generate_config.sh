0d32e292e1f64a46c9f4190a517691610c28bb73
Seth Akins <sethlinc4@gmail.com>
2026-07-08 10:55:21 -0600

    fix name and config

--- job_scripts/atari/venture_counts_sweep/generate_config.sh
+++ job_scripts/atari/venture_counts_sweep/generate_config.sh
@@ -9,6 +9,6 @@ output_file="configs.txt"
 >"$output_file"
 for beta in "${betas[@]}"; do
     for next in "${next_state[@]}"; do
-        echo "--environment venture --force-xla --beta $beta --network.next_state_coef $next --output-folder-name centure_counts_sweep/beta_${beta}_next_${next}/seed_SEED" >>"$output_file"
+        echo "one-block --environment venture --force-xla --beta $beta --network.next_state_coef $next --output-folder-name venture_counts_sweep/beta_${beta}_next_${next}/seed_SEED" >>"$output_file"
     done
 done
